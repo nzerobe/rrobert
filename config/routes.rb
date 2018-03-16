7935746c26b0624c0c8b2735c159d29e81089ad1
Nishimwe Robert <nzeroberto@gmail.com>
2018-03-16 13:04:10 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'clenie/index'
 
   #get 'robs/index'
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
  
   resources :robs
   #post 'destroy/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+ 
 end
