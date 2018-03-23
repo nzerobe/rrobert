268e4002a708e199f332a8c1fac7a34f30d3eacc
Nishimwe Robert <nzeroberto@gmail.com>
2018-03-23 08:57:59 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                 format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-                attr_accessor :password, :password_confirmation 
+                
                  before_save { email.downcase! }
                 
  validates :password, presence: true, length: { minimum: 6 }
