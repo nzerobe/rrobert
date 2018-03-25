326ff98fe6a34a9e454d9c95e4acf84c0fede2a7
Nishimwe Robert <nzeroberto@gmail.com>
2018-03-25 08:31:17 +0000

    init

--- app/models/rob.rb
+++ app/models/rob.rb
@@ -1,5 +1,11 @@
 class Rob < ApplicationRecord
     
   
-              
+                #attr_accessor :password, :password_confirmation 
+                 before_save { email.downcase! }
+                
+ 
+ #has_secure_password
+  
+  has_secure_password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-    
+    validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, length: { maximum: 255 },
+                format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                #attr_accessor :password, :password_confirmation 
+                 before_save { email.downcase! }
+                
+ validates :password, length: { minimum: 6 }
+ #has_secure_password
+  
+  has_secure_password
 end
